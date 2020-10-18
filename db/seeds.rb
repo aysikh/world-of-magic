b958466ffe1e07d373332d26d57c02fe64534534
aysikh <amy.sikhammountry@gmail.com>
2020-10-18 19:22:34 -0400

    added faker spells

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+House.destroy_all
+Spell.destroy_all
+Location.destroy_all
+Creature.destroy_all
+Encounter.destroy_all
 
 
+15.times do 
+  Spell.create(name: Faker::Movies::HarryPotter.unique.spell)
+end
