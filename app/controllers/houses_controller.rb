0bc4c7364398b275480bf102e2a87b5faac59c49
Melindadiaz07 <melindadiaz@me.com>
2020-10-21 16:09:02 -0400

    Added controller actions for fetch update requests

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -1,7 +1,7 @@
 class HousesController < ApplicationController
 
   def index
-    render json: House.all.to_json({include: {users: {only: [:name, :lives]}}, except: [:created_at, :updated_at]})
+    render json: House.all.to_json({include: {users: {only: [:id, :name, :lives]}}, except: [:created_at, :updated_at]})
   end
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,17 @@ class UsersController < ApplicationController
   def index 
     render json: User.all
   end
+
   
   def create
     user = User.create(name: params[:name], house_id: params[:house_id])
     house = House.find(params[:house_id])
+    render json: user
+  end
+
+  def update
+    user = User.find(params[:id])
+    user.update(lives: params[:lives])
     render json: user
   end
 
